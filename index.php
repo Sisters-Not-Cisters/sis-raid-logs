c13fe20ad5b91ce0d5b4c399b080b4977a80bd5f
Elizabeth Wetton <FluffyPira@users.noreply.github.com>
2017-12-06 16:56:30 -0500

    It was invalid the entire timegit add .

--- index.php
+++ index.php
@@ -22,62 +22,6 @@ $commanders = array('FluffyPira' => '<img style="height:20px; width:20px;" src="
 $classtextcolor = array("1" => "text-ele", "2" => "text-mes", "3" => "text-necro", "4" => "text-thief", "5" => "text-ranger", "6" => "text-engi", "7" => "text-guard", "8" => "text-rev", "9" => "text-war", "10" => "text-ele", "11" => "text-mes", "12" => "text-necro", "13" => "text-thief", "14" => "text-ranger", "15" => "text-engi", "16" => "text-guard", "17" => "text-rev", "18" => "text-war", "19" => "text-ele", "20" => "text-mes", "21" => "text-necro", "22" => "text-thief", "23" => "text-ranger", "24" => "text-engi", "25" => "text-guard", "26" => "text-rev", "27" => "text-war");
 
 ?>
-<script>
-	function sortTable(n) {
-		var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-		table = document.getElementById("SortTable");
-		switching = true;
-		//Set the sorting direction to ascending:
-		dir = "asc"; 
-		/*Make a loop that will continue until
-		no switching has been done:*/
-		while (switching) {
-			//start by saying: no switching is done:
-			switching = false;
-			rows = table.getElementsByTagName("TR");
-			/*Loop through all table rows (except the
-				first, which contains table headers):*/
-				for (i = 1; i < (rows.length - 1); i++) {
-					//start by saying there should be no switching:
-					shouldSwitch = false;
-					/*Get the two elements you want to compare,
-					one from current row and one from the next:*/
-					x = rows[i].getElementsByTagName("TD")[n];
-					y = rows[i + 1].getElementsByTagName("TD")[n];
-					/*check if the two rows should switch place,
-					based on the direction, asc or desc:*/
-					if (dir == "asc") {
-						if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-							//if so, mark as a switch and break the loop:
-							shouldSwitch= true;
-							break;
-						}
-					} else if (dir == "desc") {
-						if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-							//if so, mark as a switch and break the loop:
-							shouldSwitch= true;
-							break;
-						}
-					}
-				}
-				if (shouldSwitch) {
-					/*If a switch has been marked, make the switch
-					and mark that a switch has been done:*/
-					rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-					switching = true;
-					//Each time a switch is done, increase this count by 1:
-					switchcount ++;      
-				} else {
-					/*If no switching has been done AND the direction is "asc",
-					set the direction to "desc" and run the while loop again.*/
-					if (switchcount == 0 && dir == "asc") {
-						dir = "desc";
-						switching = true;
-					}
-				}
-			}
-		}
-		</script>
 		<!DOCTYPE html>
 		<html lang="en">
 		<head>
@@ -105,6 +49,7 @@ $classtextcolor = array("1" => "text-ele", "2" => "text-mes", "3" => "text-necro
 					padding: 5%;
 				}
 				</style>
+				
 			</head>
 			<body>
 				<!-- Simple Bootstrap Container. Seems popular with the other raiding guilds.-->
@@ -197,7 +142,7 @@ $classtextcolor = array("1" => "text-ele", "2" => "text-mes", "3" => "text-necro
 																$tag = $commanders[$name]." ";
 															}
 															
-															echo sprintf("<tr><td style=\"width:70%%\">%s%s</td><td tyle=\"width:30%%\">%s</td></tr>\n",
+															echo sprintf("<tr><td style=\"width:70%%\">%s%s</td><td style=\"width:30%%\">%s</td></tr>\n",
 															$tag,
 															$name,
 															$profs);
@@ -259,6 +204,62 @@ $classtextcolor = array("1" => "text-ele", "2" => "text-mes", "3" => "text-necro
 						</div>
 					</div>
 				</body>
+				<script>
+					function sortTable(n) {
+						var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+						table = document.getElementById("SortTable");
+						switching = true;
+						//Set the sorting direction to ascending:
+						dir = "asc"; 
+						/*Make a loop that will continue until
+						no switching has been done:*/
+						while (switching) {
+							//start by saying: no switching is done:
+							switching = false;
+							rows = table.getElementsByTagName("TR");
+							/*Loop through all table rows (except the
+								first, which contains table headers):*/
+								for (i = 1; i < (rows.length - 1); i++) {
+									//start by saying there should be no switching:
+									shouldSwitch = false;
+									/*Get the two elements you want to compare,
+									one from current row and one from the next:*/
+									x = rows[i].getElementsByTagName("TD")[n];
+									y = rows[i + 1].getElementsByTagName("TD")[n];
+									/*check if the two rows should switch place,
+									based on the direction, asc or desc:*/
+									if (dir == "asc") {
+										if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+											//if so, mark as a switch and break the loop:
+											shouldSwitch= true;
+											break;
+										}
+									} else if (dir == "desc") {
+										if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+											//if so, mark as a switch and break the loop:
+											shouldSwitch= true;
+											break;
+										}
+									}
+								}
+								if (shouldSwitch) {
+									/*If a switch has been marked, make the switch
+									and mark that a switch has been done:*/
+									rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+									switching = true;
+									//Each time a switch is done, increase this count by 1:
+									switchcount ++;      
+								} else {
+									/*If no switching has been done AND the direction is "asc",
+									set the direction to "desc" and run the while loop again.*/
+									if (switchcount == 0 && dir == "asc") {
+										dir = "desc";
+										switching = true;
+									}
+								}
+							}
+						}
+				</script>
 				<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
